3bb7deb89ee7dcbe56c485bbf6fc2122b8a09066
Pablo Cruz <151473902+pablo-cruzbr@users.noreply.github.com>
2025-12-28 01:04:34 -0300

    feat: estruturar lFeature: Structuring the main layout in App.jsxayout principal no App.jsx
    Importing and organizing the Home, About, Work, Testimonial, Contact, and Footer components for modular development.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
-import { useState } from 'react'
-import Home from './components/Home'
-import About from './components/About'
+import { useState } from 'react';
+import Home from './components/Home';
+import About from './components/About';
+import Testimonial from  './components/Testimonial';
+import Contact from './components/Contact';
+import Footer from './components/Footer'
 import './App.css'
 
 function App() {
@@ -10,6 +13,10 @@ function App() {
     <div>
       <Home/>
       <About/>
+      <Work/>
+      <Testimonial/>
+      <Contact/>
+      <Footer/>
     </div>
   )
 }
