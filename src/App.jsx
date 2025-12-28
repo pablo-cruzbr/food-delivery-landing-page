7a9f1b72a906c945022a7fb4f0c46b3dc8862564
Pablo Cruz <151473902+pablo-cruzbr@users.noreply.github.com>
2025-12-28 01:21:04 -0300

    feat: create new component "work"

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Home from './components/Home';
 import About from './components/About';
+import Work from './components/Work';
 import Testimonial from  './components/Testimonial';
 import Contact from './components/Contact';
 import Footer from './components/Footer'
