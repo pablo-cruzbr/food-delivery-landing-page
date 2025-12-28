a043b2256b4b5e69ea3801164289f8d4407ebfea
Pablo Cruz <151473902+pablo-cruzbr@users.noreply.github.com>
2025-12-28 11:12:39 -0300

    feat: new update section "contact"

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>
-      Contact
+   <div className="contact-page-wrapper">
+    <h1 className="primary-heading">Have Question Inn Mind?</h1>
+    <div className="contact-form-container">
+      <input type="text" placeholder='yourmail@gmail.com'/>
+      <button className="secondary-button">Submit</button>
     </div>
+   </div>
   )
 }
 
